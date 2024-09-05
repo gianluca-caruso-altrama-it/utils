d37b58591bc6ad77ca6a54d1be55afa4321062d6
Gianluca Caruso <160881689+gianluca-caruso-altrama-it@users.noreply.github.com>
2024-09-05 15:33:14 +0200

    Update create-new-user.sh

--- create-new-user.sh
+++ create-new-user.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 # get user
-user = $1
-# add user, create a new home for user and new password
-sudo adduser --ingroup sudo --home /home/$user --shell /bin/bash $user && passwd -e $user
-# refresh shell
-newgrp sudo
+user=$1
+full_name=$2
+password=$3
+
+# Controlla se la password è vuota, altrimenti genera una password casuale
+if [ -z "$password" ]; then
+  # Genera una password sicura con lettere, numeri e simboli speciali (16 caratteri)
+  password=$(tr -dc 'A-Za-z0-9!@#$%^&*()_+{}|:<>?=,./;' < /dev/urandom | head -c16)
+  echo "Generated secure password for $user: $password"
+else
+  echo "Using provided password for $user"
+fi
+
+echo "Creating new user: $user"
+
+# Add user, set full name, home directory, and shell without interaction
+sudo useradd -m -d "/home/$user" -s /bin/bash -c "$full_name" "$user"
+
+# Set password without interaction
+echo "$user:$password" | sudo chpasswd
+
+# Add user to the sudo group
+sudo usermod -aG sudo "$user"
+
+echo "User $user created with full name: $full_name and password: $password"
