f21c7e56f638f47f81bfa8aafe51b97d47e8a6ac
Gianluca Caruso <160881689+gianluca-caruso-altrama-it@users.noreply.github.com>
2024-09-05 15:49:22 +0200

    Update docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# update before run script
+sudo apt update -y
+sudo apt upgrade -y
+
 # Add Docker's official GPG key:
-sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -11,4 +15,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+
+# system update
+sudo apt update -y
+sudo apt upgrade -y
